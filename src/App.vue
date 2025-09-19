054cbba335dc9c5110f0f29a208d4ff43852b0e3
Kyaw Myo Htay <kyawmyohtay2004@gmail.com>
2025-09-19 19:28:17 +0630

    Vue Cli3 template refs and what is components

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,19 @@
   <h1 ref="greet">Hi {{ title }}i</h1>
   <input type="text" ref="name" />
   <button @click="clickHandler">Click</button>
+  <Modal />
 </template>
 
 <script>
+import Modal from "./components/CustomModal.vue";
 export default {
   data() {
     return {
       title: "Vue 3",
     };
+  },
+  components: {
+    Modal,
   },
   methods: {
     clickHandler() {
